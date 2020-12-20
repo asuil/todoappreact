fb6460bd6c5924c45cb7050d6af39d4221567b3b
Fernando Martinez <femartinez@uc.cl>
2020-12-20 15:37:57 -0300

    fix: margin y ejemplo separacion de componente Todo item

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import './App.css';
-
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Todo from './components/Todo';
 
 
 // material-ui/core styles
 const useStyles = makeStyles({
   cardcontainer: {
     padding: '2rem 10rem',
-  },
-  todotitle: {
-    fontSize: '20px',
-  },
-  todosubtitle: {
-    color: '#aaaaaa',
-  },
-  todobutton: {
-    margin: '3px',
-  },
-  todocardgrid: {
-    padding: '20px',
   },
   inputcard: {
     padding: '20px',
@@ -37,58 +22,6 @@ const useStyles = makeStyles({
     flexDirection: 'row-reverse',
   },
 });
-
-
-/**
- * Todo Component
- * @param todo: {text: todo content, isDone: true/false depending on todo state}
- * @param index: used to identify this todo
- * @param completeTodo: function to execute when clicking "done"
- * @param removeTodo: function to execute when clicking "delete"
- * @returns individual todo div
- */
-function Todo({ todo, index, completeTodo, removeTodo }) {
-
-  // global styles
-  const classes = useStyles();
-
-  return (
-      <Grid item xs={6}>
-        <Card>
-          <Grid container className={classes.todocardgrid}>
-            <Grid item xs={6}>
-              <Typography className={classes.todotitle} style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
-                {todo.text}
-              </Typography>
-              <Typography className={classes.todosubtitle} style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
-                {todo.desc}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <CardActions className={classes.buttoncontainer}>
-                <Button
-                    className={classes.todobutton}
-                    variant="contained"
-                    color="primary"
-                    onClick={() => completeTodo(index)}
-                >
-                  Done!
-                </Button>
-                <Button
-                    className={classes.todobutton}
-                    variant="contained"
-                    color="primary"
-                    onClick={() => removeTodo(index)}
-                >
-                  Delete
-                </Button>
-              </CardActions>
-            </Grid>
-          </Grid>
-        </Card>
-      </Grid>
-  );
-}
 
 
 /**
---
+++ src/components/Todo.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import { 
+  Grid,
+  Button,
+  Card,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  todocardgrid: {
+    padding: '20px',
+  },
+  todotitle: {
+    fontSize: '20px',
+  },
+  todosubtitle: {
+    color: '#aaaaaa',
+  },
+  todobutton: {
+    margin: '3px',
+  },
+  buttoncontainer: {
+    display: 'flex',
+    flexDirection: 'row-reverse',
+  },
+});
+
+/**
+ * Todo Component
+ * @param todo: {text: todo content, isDone: true/false depending on todo state}
+ * @param index: used to identify this todo
+ * @param completeTodo: function to execute when clicking "done"
+ * @param removeTodo: function to execute when clicking "delete"
+ * @returns individual todo div
+ */
+function Todo({ todo, index, completeTodo, removeTodo }) {
+
+  // global styles
+  const classes = useStyles();
+
+  return (
+      <Grid item xs={6}>
+        <Card>
+          <Grid container className={classes.todocardgrid}>
+            <Grid item xs={6}>
+              <Typography className={classes.todotitle} style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+                {todo.text}
+              </Typography>
+              <Typography className={classes.todosubtitle} style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+                {todo.desc}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <CardActions className={classes.buttoncontainer}>
+                <Button
+                    className={classes.todobutton}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => completeTodo(index)}
+                >
+                  Done!
+                </Button>
+                <Button
+                    className={classes.todobutton}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => removeTodo(index)}
+                >
+                  Delete
+                </Button>
+              </CardActions>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+  );
+}
+
+export default Todo;
